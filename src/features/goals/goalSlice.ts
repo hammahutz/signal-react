a0d44c904fabfe1b416dea028b98f90a43548a95
hammahutz <62766637+hammahutz@users.noreply.github.com>
2023-12-31 11:35:44 +0100

    Pre prettier

---
+++ src/features/goals/goalSlice.ts
@@ -0,0 +1,8 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+const initialState = {};
+
+export const goalSlice = createSlice({
+    name: "goal",
+    initialState,
+    reducers: {},
+});
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,15 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAppSelector } from "../app/hooks"
 
 const Dashboard: React.FC = () => {
+  const navigate = useNavigate()
+  const { user } = useAppSelector((state) => state.auth)
+  useEffect(() => {
+    if (!user) {
+      navigate("/login")
+    }
+  }, [user, navigate])
   return <div>Dashboard</div>
 }
 
